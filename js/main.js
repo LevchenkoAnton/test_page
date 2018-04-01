656c1ed83bed5091c15b2071136f440726cf94d9
Anton Levchenko <levchik03021998@gmail.com>
2018-04-01 16:43:27 +0300

    animation for popup was done

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ let contactCloseBtn = document.querySelector('.contact-close');
 let contactForm = document.querySelector('.contact-popup');
 let contactName = document.querySelector('.input-name');
 
+let openNavBtn = document.querySelector('.open-nav-btn');
+let headerNav = document.querySelector('.nav-list');
+
+let form = document.querySelector('.contact-form');
+
 searchOpenBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     searchPopup.classList.add('open-popup');
@@ -37,12 +42,42 @@ contactOpenBtn.addEventListener('click', function (evt) {
 contactCloseBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     contactPopup.classList.remove('open-popup');
+    contactPopup.classList.remove('error-form');
 });
 
 window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
         if (contactPopup.classList.contains('open-popup')) {
-            contactPopup.classList.remove('open-popup')
+            contactPopup.classList.remove('open-popup');
+            contactPopup.classList.remove('error-form');
         }
     }
 });
+
+openNavBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    headerNav.classList.toggle('open-nav')
+});
+
+form.addEventListener('submit', function (evt) {
+    let inputName = document.querySelector('.input-name');
+    let inputPhone = document.querySelector('.input-phone');
+    let inputEmail = document.querySelector('.input-email');
+    let contactMassage = document.querySelector('.contact-massage');
+    if (!(inputName.value && inputEmail.value && inputPhone.value && contactMassage.value)) {
+        evt.preventDefault();
+        contactPopup.classList.add('error-form');
+        alert('Заполните поля формы');
+    } else {
+        if (!inputName.match('^[a-zA-Z]+$')){
+            evt.preventDefault();
+            contactPopup.classList.add('error-form');
+            alert('В поле имя не должно быть цыфр');
+        }
+    }
+});
+
+$(function($){
+    $('.input-phone').mask("+9(999) 999-99-99");
+});
+
